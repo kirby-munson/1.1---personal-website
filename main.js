fa8bec1d6c969265fd32e2529a165188749d6d2a
Kirby Munson <kmunson@g.clemson.edu>
2016-05-16 15:20:55 -0400

    Second commit with styles

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// alert('I love bacon');
+var r = confirm("If you love superhero's press OK");
+if (r === true) {
+  prompt("Who is your favorite superhero?");
+} else {
+  alert("You should learn to love them here!");
+}
