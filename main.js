b1c053fbefbbb10674e9feb5d6adb0e68b70ed3d
Kirby Munson <kmunson@g.clemson.edu>
2016-05-17 09:17:45 -0400

    Final commit of personal website

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var r = confirm("If you love superhero's press OK");
-if (r === true) {
-  prompt("Who is your favorite superhero?");
-} else {
-  alert("You should learn to love them here!");
+function changeimage() {
+  var image = imageById('myPic');
+  if (image.src.match("super")) {
+    image.src = "https://pixabay.com/static/uploads/photo/2013/07/12/17/58/heroes-152707_960_720.png";
+  } else {
+    image.src = "http://images.clipartpanda.com/superman-black-and-white-nTXqg8jTB.png"
+  }
+  }
 }
